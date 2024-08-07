8f916baa834df16f68b9964b563b4f36072c4fc1
bot19 <jsbot@pm.me>
2024-08-07 23:48:32 +1000

    prettier + remove avatar_url - don't care yet

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,71 +1,72 @@
-import { useState, useEffect } from 'react'
-import { supabase } from './supabaseClient'
-import Avatar from './Avatar'
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+// import Avatar from './Avatar'
 
 export default function Account({ session }) {
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState(null)
-  const [website, setWebsite] = useState(null)
-  const [avatar_url, setAvatarUrl] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [link, setLink] = useState(null);
+  // const [avatar_url, setAvatarUrl] = useState(null)
 
   useEffect(() => {
     async function getProfile() {
-      setLoading(true)
-      const { user } = session
+      setLoading(true);
+      const { user } = session;
 
       let { data, error } = await supabase
-        .from('profiles')
-        .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single()
+        .from("profiles")
+        .select(`username, link`)
+        .eq("id", user.id)
+        .single();
 
       if (error) {
-        console.warn(error)
+        console.warn(error);
       } else if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setLink(data.link);
+        // setAvatarUrl(data.avatar_url)
       }
 
-      setLoading(false)
+      setLoading(false);
     }
 
-    getProfile()
-  }, [session])
+    getProfile();
+  }, [session]);
 
-  async function updateProfile(event, avatarUrl) {
-    event.preventDefault()
+  // (event, avatarUrl)
+  async function updateProfile(event) {
+    event.preventDefault();
 
-    setLoading(true)
-    const { user } = session
+    setLoading(true);
+    const { user } = session;
 
     const updates = {
       id: user.id,
       username,
-      website,
-      avatar_url: avatarUrl,
+      link,
+      // avatar_url: avatarUrl,
       updated_at: new Date(),
-    }
+    };
 
-    let { error } = await supabase.from('profiles').upsert(updates)
+    let { error } = await supabase.from("profiles").upsert(updates);
 
     if (error) {
-      alert(error.message)
+      alert(error.message);
     } else {
-      setAvatarUrl(avatarUrl)
+      // setAvatarUrl(avatarUrl)
     }
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
     <form onSubmit={updateProfile} className="form-widget">
-      <Avatar
+      {/* <Avatar
         url={avatar_url}
         size={150}
         onUpload={(event, url) => {
           updateProfile(event, url)
         }}
-      />
+      /> */}
       <div>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" value={session.user.email} disabled />
@@ -76,31 +77,39 @@ export default function Account({ session }) {
           id="username"
           type="text"
           required
-          value={username || ''}
+          value={username || ""}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="website">Website</label>
+        <label htmlFor="link">Link</label>
         <input
-          id="website"
+          id="link"
           type="url"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
+          value={link || ""}
+          onChange={(e) => setLink(e.target.value)}
         />
       </div>
 
       <div>
-        <button className="button block primary" type="submit" disabled={loading}>
-          {loading ? 'Loading ...' : 'Update'}
+        <button
+          className="button block primary"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
         </button>
       </div>
 
       <div>
-        <button className="button block" type="button" onClick={() => supabase.auth.signOut()}>
+        <button
+          className="button block"
+          type="button"
+          onClick={() => supabase.auth.signOut()}
+        >
           Sign Out
         </button>
       </div>
     </form>
-  )
+  );
 }
