6148c7b90c49a61451826b08588cb055ab4b3926
bot19 <jsbot@pm.me>
2024-08-09 13:07:50 +1000

    move styling to /styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./styles/App.css";
 import { supabase } from "./supabaseClient";
 import Auth from "./Auth";
 import Account from "./Account";
